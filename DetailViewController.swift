48be8af1949d08a736e9ad8bac7f04d18e612ace
Brendan Giberson <bgibers@clemson.edu>
2019-07-10 23:56:13 -0400

    Working table view w selectable images

---
+++ DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Day1-StormViewer
+//
+//  Created by Brendan Giberson on 7/10/19.
+//  Copyright © 2019 BrendanGiberson. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+  
+  @IBOutlet var imageView: UIImageView!
+  var selectedImage: String?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    if let imageToLoad = selectedImage {
+      imageView.image  = UIImage(named: imageToLoad)
+    }
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
